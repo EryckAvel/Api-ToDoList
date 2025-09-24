18d90ea2f7561fd1f8417a8cf91baa4a9314de17
Eryck Avel <eryckavel@gmail.com>
2025-09-23 22:00:50 -0300

    feat: Implementando logica para tratative de exceptions

---
+++ src/main/java/com/eryckavel/todolist/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.eryckavel.todolist.exception;
+
+import com.eryckavel.todolist.exception.dto.ErrorResponse;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getDescription(false).replace("uri=", "")
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
